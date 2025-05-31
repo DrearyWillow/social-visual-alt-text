7ddf2b0a503c717c60dd2152fdeb5b2426beee7d
MellowKyler <dewerdk@gmail.com>
2025-05-31 09:03:49 -0500

    gif ai alt text

--- src/bluesky.js
+++ src/bluesky.js
@@ -88,6 +88,16 @@ let insertAlt = function () {
             ) {
                 altText.style.backgroundColor = options.colorNoAlt;
                 altText.style.height = "12px";
+            } else if (
+                !userImage.parentElement.querySelector('button[data-testid="altTextButton"]')
+            ) {
+                altText.style.color = options.colorAltText;
+                altText.style.backgroundColor = options.aiColorAltBg;
+                altText.style.fontSize = "18px";
+                altText.style.padding = "4px 8px";
+                altText.style.fontFamily =
+                    'Arial, "Helvetica Neue", Helvetica, sans-serif';
+                altText.insertAdjacentHTML('beforeend', newlineToBr(userImage.getAttribute("aria-label")));
             } else {
                 altText.style.color = options.colorAltText;
                 altText.style.backgroundColor = options.colorAltBg;
